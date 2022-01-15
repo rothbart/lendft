4bfa60815c6b4b107b9b41f252d281cd28af2381
Andrew Rothbart <rothbart@gmail.com>
2022-01-14 20:02:27 -0800

    better detection logic for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { ethers } from "ethers";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { ExternalProvider } from "@ethersproject/providers";
+
+enum WalletState {
+  Loading,
+  Loaded,
+  NotFound,
+}
 
 function App() {
   const [address, setAddress] = useState("");
+  const [walletState, setWalletState] = useState(WalletState.Loading);
 
   useEffect(() => {
-    const ethereum = (window as any).ethereum;
-    if (!ethereum) {
-      return;
-    }
-
     const getWallet = async () => {
-      const provider = new ethers.providers.Web3Provider(ethereum, "any");
-      await provider.send("eth_requestAccounts", []);
-      const address = await provider.getSigner().getAddress();
-      setAddress(address);
+      try {
+        const ethereum = (await detectEthereumProvider()) as ExternalProvider;
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        await provider.send("eth_requestAccounts", []);
+        const address = await provider.getSigner().getAddress();
+        setAddress(address);
+        setWalletState(WalletState.Loaded);
+      } catch (err) {
+        setWalletState(WalletState.NotFound);
+      }
     };
 
     getWallet();
@@ -23,13 +33,17 @@ function App() {
 
   return (
     <div className="App">
-      {address ? (
-        <p>
-          <strong>Wallet address:</strong> {address}
-        </p>
-      ) : (
-        <p>Fetching wallet..</p>
-      )}{" "}
+      {
+        {
+          [WalletState.NotFound]: <p>No wallet detected</p>,
+          [WalletState.Loaded]: (
+            <p>
+              <strong>Wallet address:</strong> {address}
+            </p>
+          ),
+          [WalletState.Loading]: <p>Loading wallet..</p>,
+        }[walletState]
+      }
     </div>
   );
 }
