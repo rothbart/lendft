6714b7ae6ff219877be948be868ce5fa6d495267
Andrew Rothbart <rothbart@gmail.com>
2022-01-16 16:51:06 -0800

    test network and some refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { ethers } from "ethers";
-import detectEthereumProvider from "@metamask/detect-provider";
-import { ExternalProvider } from "@ethersproject/providers";
-import axios, { AxiosRequestConfig } from "axios";
 import qs from "qs";
-
-async function getNFTs(owner: string) {
-  // https://docs.opensea.io/reference/getting-assets
-  var config: AxiosRequestConfig = {
-    method: "get",
-    url: `https://api.opensea.io/api/v1/assets`,
-    params: {
-      owner,
-      offset: 0,
-      limit: 50,
-    },
-  };
-
-  const res = await axios(config);
-
-  const tokenData: any[] = res.data?.assets || [];
-
-  return tokenData.map((tokenData) => ({
-    name: tokenData?.name,
-    id: tokenData?.id,
-    image_url: tokenData?.image_thumbnail_url,
-    permalink: tokenData?.permalink,
-    contract_address: tokenData?.asset_contract?.address,
-    type: tokenData?.asset_contract?.asset_contract_type,
-  }));
-}
-type NFTList = Awaited<ReturnType<typeof getNFTs>>;
+import { useWallet } from "./WalletProvider";
+import { getNFTs, NFTList } from "./helpers";
 
 function renderNFTs(nfts: NFTList) {
   if (!nfts.length) {
@@ -82,46 +53,44 @@ function renderNFTs(nfts: NFTList) {
   );
 }
 
-enum WalletState {
+enum NFTState {
   Loading,
   Loaded,
   NotFound,
 }
 
 function App() {
-  const [address, setAddress] = useState("");
+  const wallet = useWallet();
   const [nfts, setNFTs] = useState<NFTList>([]);
-  const [walletState, setWalletState] = useState(WalletState.Loading);
+  const [walletState, setWalletState] = useState(NFTState.Loading);
 
   const params = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+  const addressOverride = params.address as string;
 
   useEffect(() => {
-    const getWallet = async () => {
-      try {
-        const ethereum = (await detectEthereumProvider()) as ExternalProvider;
-        const provider = new ethers.providers.Web3Provider(ethereum, "any");
-        await provider.send("eth_requestAccounts", []);
-        const address = await provider.getSigner().getAddress();
-        const nfts = await getNFTs((params.address as string) || address);
-
+    const getNFTList = async () => {
+      if (wallet) {
+        const nfts = await getNFTs(
+          addressOverride || wallet.address,
+          wallet.isRinkeby
+        );
         setNFTs(nfts);
-        setAddress(address);
-        setWalletState(WalletState.Loaded);
-      } catch (err) {
-        setWalletState(WalletState.NotFound);
+        setWalletState(NFTState.Loaded);
+      } else {
+        setWalletState(NFTState.NotFound);
       }
     };
 
-    getWallet();
-  }, []);
+    getNFTList();
+  }, [wallet, addressOverride]);
 
   return (
     <div className="app">
       {
         {
-          [WalletState.NotFound]: <p>No wallet detected</p>,
-          [WalletState.Loaded]: renderNFTs(nfts),
-          [WalletState.Loading]: <p>Loading wallet..</p>,
+          [NFTState.NotFound]: <p>No wallet detected</p>,
+          [NFTState.Loaded]: renderNFTs(nfts),
+          [NFTState.Loading]: <p>Loading wallet..</p>,
         }[walletState]
       }
     </div>
---
+++ src/WalletProvider.tsx
@@ -0,0 +1,63 @@
+import { ExternalProvider } from "@ethersproject/providers";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { ethers } from "ethers";
+import React, {
+  createContext,
+  useContext,
+  FunctionComponent,
+  useEffect,
+  useState,
+} from "react";
+
+interface WalletContextType {
+  provider: ethers.providers.Web3Provider;
+  address: string;
+  isRinkeby: boolean;
+}
+const WalletContext = createContext<WalletContextType | null>(null);
+export const useWallet = () => useContext(WalletContext);
+
+interface WalletProviderProps {
+  children: any;
+}
+const WalletProvider: FunctionComponent<WalletProviderProps> = (props) => {
+  const [loaded, setLoaded] = useState(false);
+  const [walletState, setWalletState] = useState<WalletContextType | null>(
+    null
+  );
+
+  useEffect(() => {
+    const getWallet = async () => {
+      try {
+        const ethereum = (await detectEthereumProvider()) as ExternalProvider;
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        await provider.send("eth_requestAccounts", []);
+        const address = await provider.getSigner().getAddress();
+        const network = await provider.getNetwork();
+
+        setWalletState({
+          provider,
+          address,
+          isRinkeby: network.name === "rinkeby",
+        });
+      } catch (err) {
+      } finally {
+        setLoaded(true);
+      }
+    };
+
+    getWallet();
+  }, []);
+
+  if (!loaded) {
+    return <React.Fragment />;
+  }
+
+  return (
+    <WalletContext.Provider value={walletState}>
+      {props.children}
+    </WalletContext.Provider>
+  );
+};
+
+export default WalletProvider;
